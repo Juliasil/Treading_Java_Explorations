ae02afa28e7c1e081969a8e0660e2e7ce2c0938c
Juliana da Silveira Aguilar <julianasilveiraaguilar@gmail.com>
2024-05-23 23:05:19 -0300

    Update to ExeSomaArray.java

--- matriz/ExeSomaArray.java
+++ matriz/ExeSomaArray.java
@@ -13,7 +13,7 @@ package matriz;
 public class ExeSomaArray {
   public static void main(String[] args) {
   int[][] allNumbers = {
-    {1, 2, 3, 4, 5},
+    {1, 2, 3, 4, 5,},
     {-1, 1, 0, 5, -5}
   };
 

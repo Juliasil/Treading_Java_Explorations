7c0813472259a828b09d554636bd09f71d2ee9b6
Juliana da Silveira Aguilar <julianasilveiraaguilar@gmail.com>
2024-05-25 20:20:35 -0300

    Implements sum method to calculate the sum of array elements

--- matriz/ExeSomaArray.java
+++ matriz/ExeSomaArray.java
@@ -12,17 +12,24 @@ package matriz;
 
 public class ExeSomaArray {
   public static void main(String[] args) {
-  int[][] allNumbers = {
-    {1, 2, 3, 4, 5,},
-    {-1, 1, 0, 5, -5}
-  };
+    int[][] allNumbers = {
+        {1, 2, 3, 4, 5},    
+        {-1, 1, 0, 5, -5}   
+    };
 
-  for(int[] numbers : allNumbers) {
-    int sum = 0;
-    for (int number : numbers) {
-      sum += number;
-      }
-      System.out.println("Sum: " + sum);
+    for (int[] numbers : allNumbers) {
+        int sum = soma(numbers);
+        System.out.println("Sum: " + sum);
     }
   }
+
+  private static int soma(int[] numbers) {
+    int soma = 0;
+    for (int number : numbers) {
+        soma += number;
+    }
+    return soma;
+  }
 }
+
+
